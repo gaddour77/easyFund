5db413439bc6f617a416c9e2431e524c18a59f62
gaddour77 <mohamedabdelkader.benfraj@esprit.tn>
2024-04-01 05:22:11 +0000

    stripe api

---
+++ src/main/java/tn/esprit/easyfund/dto/StripeTokenDto.java
@@ -0,0 +1,13 @@
+package tn.esprit.easyfund.dto;
+
+import lombok.Data;
+@Data
+public class StripeTokenDto {
+    private String cardNumber;
+    private String expMonth;
+    private String expYear;
+    private String cvc;
+    private String token;
+    private String username;
+    private boolean succes;
+}
